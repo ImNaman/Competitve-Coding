48c5c57a33cd78406faf713c17e343909bf463a6
ImNaman <namanaol1710@gmail.com>
2017-04-28 10:17:40 +0530

    Adding Matrix Expo

---
+++ KMP/Kmp.cpp
@@ -0,0 +1,69 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+
+int lps[100005];
+
+void LPS(string str)
+{
+	ll p1=1,p2=0;
+	lps[0] = 0;	
+	ll n = str.length();
+	
+	while(p1<n)
+	{
+		if(str[p1]==str[p2])
+		{
+			lps[p1] = lps[p1-1]+1;
+			p1+=1;
+			p2+=1;
+		}
+		else
+		{
+			if(p2==0)
+			{
+				lps[p1] = 0;
+				p1+=1;
+			}
+			else
+			{
+				p2 = lps[p2-1];
+			}
+		}
+	}
+}
+
+void KMP(string str,string pat)
+{
+	ll n1 = str.length();
+	ll n2 = pat.length();
+
+	LPS(pat);
+
+	ll p1=0,p2=0;	
+
+	while(p1<n1)
+	{
+		if(str[p1]==pat[p2])
+			p1+=1,p2+=1;
+
+		if(p2==n2)
+			p2 = lps[p2-1],cout<<"Pattern found "<<p1-p2<<endl;
+
+		else if(p1<n1 && str[p1]!=pat[p2])
+		{
+			if(p2!=0)
+				p2 =  lps[p2-1];
+			else
+				p1+=1;
+		}
+	}
+}
+
+
+int main()
+{	
+	string a = "ABABABCABABABCABABABC";
+	string b = "ABABA";
+	KMP(a,b);
+}	
---
+++ Matrix Exponentiation/MatrixExpo.cpp
@@ -0,0 +1,88 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef int ll;
+
+class matrix
+{
+public:
+	ll **a;
+	ll n;
+
+	matrix(ll g=2)
+	{
+		n = g;
+		a = new ll*[n];
+		for(ll i=0;i<n;i++)
+			a[i] = new ll[n];
+
+		for(ll i=0;i<n;i++)
+			for(ll j=0;j<n;j++)
+				a[i][j] = 0;
+	}
+	void identity()
+	{
+		for(ll i=0;i<n;i++)
+			for(ll j=0;j<n;j++)
+				a[i][j] = (i==j);
+	}
+	void print()
+	{
+		for(ll i=0;i<n;i++)
+		{
+			for(ll j=0;j<n;j++)
+				cout<<a[i][j]<<" ";cout<<"\n";
+		}
+	}
+};
+
+matrix mul(matrix A, matrix B)
+{
+	matrix ans(A.n);
+	ll sz = A.n;
+	for(ll i=0;i<sz;i++)
+	{
+		for(ll j=0;j<sz;j++)
+		{
+			for(ll k=0;k<sz;k++)
+			{
+				ans.a[i][j] += A.a[i][k]*B.a[k][j];
+			}
+		}
+	}
+	return ans;
+}
+
+
+matrix expo(matrix M, ll k)
+{
+	matrix N(M.n);
+	N = M;
+	matrix ans(M.n);
+	ans.identity();
+
+	for (; k; k >>= 1) 
+	{
+		if (k & 1) 
+			ans = mul(ans,N) ; 
+		N = mul(N,N) ;
+	}
+	return ans;	
+}
+
+
+int main()
+{
+	// Example to print the nth Fibonacci number  1 , 1 , 2 , 3....
+	// a(n) = a(n-1) + a(n-2)
+
+	matrix a(2);
+
+	a.a[0][0] = 1;
+	a.a[0][1] = 1;
+	a.a[1][0] = 1;
+
+	matrix ans = expo(a,22);  // To print the 22nd fibo numer
+	cout<<ans.a[0][0]<<endl;	
+		
+	return 0;
+}
