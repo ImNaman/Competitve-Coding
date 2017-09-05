7c7fa86f0d884443530a25e9a7fe5fefbe6b3d65
ImNaman <namanaol1710@gmail.com>
2017-09-05 12:34:04 +0530

    Modified trie

--- Trie/trie.cpp
+++ Trie/trie.cpp
@@ -7,51 +7,34 @@ typedef long long int ll;
 
 class node
 {
-public:
-	char ch;  // to denote the character this node denotes
-	node* a[26]; // To store addresses of next 26 nodes
+	unordered_map<char,node* >a; // To store addresses of child nodes
 	int end; // A var to store the number of strings which end on this node
 
 	node()  // constructor
 	{
-		for(ll i=0;i<26;i++)
-			a[i] = NULL;
 		end=0;
 	}
-	void insert(string str);  // Function to insert all this strings into the trie
+	void insert(node* p , string str, int cur, int str_len);  // Function to insert all this strings into the trie
 	int search(string str);   // Function to query a string
 };
 
-void node::insert(string str)
+void node::insert(node* p , string str, int cur, int str_len)
 {
-	node *p = this;
-	node *q = p;
-	ll ptr=0,strlen=str.length();
-
-	if(p->a[str[ptr]-'a'] != NULL)
-	while(p!=NULL && ptr<strlen)
+	if(cur==str_len)
 	{
-		q = p;
-		p = p->a[str[ptr]-'a'];
-		ptr+=1;
+		p->end+=1;return;
 	}
+
+	char ch = str[cur];
 	
-	if(ptr==strlen)
-	{	(p->end)+=1;	return;	}
+	if (p->a[ch] == NULL)
+    {
+        p->a[ch] = new node();
+    }
+	insert(p->a[ch], str , cur+1,str_len);
 
-	for(ll i=ptr;i<strlen;i++)
-	{
-		node *nw = new node();
-		nw->ch = str[i];
-
-		if(i==strlen-1)
-			(nw->end)+=1;
-
-		q->a[str[i]-'a'] = nw;
-		q = q->a[str[i]-'a'];
-	}
 }
-	
+
 int node::search(string str)
 {	
 	node *p = this;
@@ -59,30 +42,13 @@ int node::search(string str)
 	{
 		if(p==NULL)
 			return 0;
-		p = p->a[str[i]-'a'];
+		p = p->a[str[i]];
 	}
 	return p->end;
 }	
 
 int main()
 {
-	node *root = new node();
-	// cout<<"Enter the number of Strings and the number of queries you wanna perform\n";
-	int n,q;cin>>n>>q;
-
-	string s[n];
-	// cout<<"Enter the input strings\n";
-	for(ll i=0;i<n;i++)
-	{
-		cin>>s[i];
-		root->insert(s[i]);
-	}
-	//cout<<"Enter the query strings\n";
-	while(q--)
-	{
-		string str;cin>>str;
-		cout<<root->search(str)<<"\n";
-	}
-
+	ios_base::sync_with_stdio(0);cin.tie(0);
 	return 0;
 }
